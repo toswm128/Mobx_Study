1d263038affe7789c7b561245af7760bc8bd6990
조민수 <chominsu10@gmail.com>
2021-09-01 14:10:39 +0900

    bable

--- mobx/src/components/App.js
+++ mobx/src/components/App.js
@@ -5,23 +5,7 @@ import Serve from "../pages/Serve";
 import Store, { ListContext } from "../Store/Store";
 
 function App() {
-  const [list, setList] = useState([
-    {
-      title: "제목1",
-      content: "내용1",
-      writer: "작성자1",
-    },
-    {
-      title: "제목2",
-      content: "내용2",
-      writer: "작성자2",
-    },
-    {
-      title: "제목3",
-      content: "내용3",
-      writer: "작성자3",
-    },
-  ]);
+  const [list, setList] = useState([]);
   const handleList = list => {
     setList(list);
   };
